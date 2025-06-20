d10ac730d33317f14a61ceb95bf9f3d4166ef38e
Sharmin <sharmin1@pichu.the-lab.llnl.gov>
2025-06-20 15:30:01 -0700

    cancellation test case

---
+++ tests/cpu_checking/dynamic/test_fp32_roundingError_cancellation/compute.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "compute.h"
+using namespace std;
+
+float difference_of_squares(float x, float y) {
+    // float a  = x * x;
+    // float b = y * y;
+    // return a - b;
+     return x * x - y * y;
+}
+
+// float stable_difference_of_square(float x, float y){
+//     float a  = x + y;
+//     float b = x - y;
+//     return a * b;
+//     // return (x + y) * (x - y);
+// }
---
+++ tests/cpu_checking/dynamic/test_fp32_roundingError_cancellation/compute.h
@@ -0,0 +1,3 @@
+
+float difference_of_squares(float x, float y);
+// float stable_difference_of_square(float x, float y);
