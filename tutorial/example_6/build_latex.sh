76c0dd083d453fae65f731c6e575a6b7264a345d
Ignacio Laguna <ilaguna@llnl.gov>
2026-04-09 09:12:56 -0700

    Minor changes to example 6

--- tutorial/example_6/build_latex.sh
+++ tutorial/example_6/build_latex.sh
@@ -1,6 +1,7 @@
 #!/bin/bash 
 
-rm -rf *.log *.aux *.out *.toc
+pdflatex example6_math_formulas.tex
+pdflatex example6_math_formulas.tex
 
-pdflatex example6_math_formulas.tex
-pdflatex example6_math_formulas.tex
+# Keep PDF only
+rm -rf *.log *.aux *.out *.toc
